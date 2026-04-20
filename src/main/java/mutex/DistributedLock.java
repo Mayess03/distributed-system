d3e8da11ea1ef41203a571490d03c219014e90f0
eyamakni <makni.eya03@gmail.com>
2026-04-20 14:58:34 +0100

    changement

--- src/main/java/mutex/DistributedLock.java
+++ src/main/java/mutex/DistributedLock.java
@@ -1,16 +1,25 @@
 package mutex;
 
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.Set;
 
 public class DistributedLock {
 
     private static final ConcurrentHashMap<String, Boolean> locks = new ConcurrentHashMap<>();
+    private static final Set<String> executedTasks = ConcurrentHashMap.newKeySet();
 
     public static boolean acquire(String taskId) {
+
+        if (executedTasks.contains(taskId)) {
+            return false;
+        }
+
         return locks.putIfAbsent(taskId, true) == null;
     }
 
     public static void release(String taskId) {
         locks.remove(taskId);
+
+        executedTasks.add(taskId);
     }
 }
